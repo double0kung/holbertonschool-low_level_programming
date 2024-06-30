12f84ab5e2f3cd08953442dda78d4986d9c642ed
double0kung <00kung@gmail.com>
2024-06-30 00:58:27 +0000

    added _putchar.c

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,5 +1,9 @@
 #include "main.h"
-/** main - Entry pointmain - Prints "_putchar" followed by a new line*/
+/**
+* main - Prints "_putchar" followed by a new line
+*
+* Return: Always 0
+*/
 int main(void)
 {
 char str[] = "_putchar";
