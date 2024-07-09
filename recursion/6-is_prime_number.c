61e2970d81cea2eeca75858e4389d221ae6c3483
double0kung <00kung@gmail.com>
2024-07-09 11:17:29 +0000

    update file 6

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -13,10 +13,8 @@ if (n <= 1)
 return (0);
 return (is_prime_helper(n, 2));
 }
-
 /**
  * is_prime_helper - helper function to check for prime number.
- * 
  * @i: the current divisor to check.
  * Return: 1 if n is a prime number, 0 otherwise.
  */
